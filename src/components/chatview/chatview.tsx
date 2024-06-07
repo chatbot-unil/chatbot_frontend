539de9d4163c596600fc515b0f5f30d54cbf7c53
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-07 14:24:19 +0200

    add sub docker compose & material ui componnents

--- src/components/chatview/chatview.tsx
+++ src/components/chatview/chatview.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 import io, { Socket } from 'socket.io-client';
-import './chatview.css';
-import Message, { MessageType, MessageStatus, MessageProps } from '../message/message';
-import IconButton from '@mui/material/IconButton';
+import { Box, IconButton, TextareaAutosize } from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import SendIcon from '@mui/icons-material/Send';
+import Message, { MessageType, MessageStatus, MessageProps } from '../message/message';
+import './chatview.css';
 
 const SERVER_URL = `http://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`;
 
@@ -20,7 +20,7 @@ const ChatView: React.FC = () => {
   const observerRef = useRef<IntersectionObserver | null>(null);
 
   useEffect(() => {
-	const socket = io(SERVER_URL);
+    const socket = io(SERVER_URL);
     socketRef.current = socket;
 
     socket.on('connect', () => {
@@ -33,14 +33,14 @@ const ChatView: React.FC = () => {
     });
 
     socket.on('session_init', (data) => {
-		localStorage.setItem('sessionId', data.session_id);
-		setSessionId(data.session_id);
-		setMessages((prevMessages) => [
-			...prevMessages,
-			{ type: MessageType.Bot, content: data.initial_message },
-		]);
-		console.log(`Session initialisée avec l'ID: ${data.session_id}`);
-	});
+      localStorage.setItem('sessionId', data.session_id);
+      setSessionId(data.session_id);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: MessageType.Bot, content: data.initial_message },
+      ]);
+      console.log(`Session initialisée avec l'ID: ${data.session_id}`);
+    });
 
     socket.on('session_restored', (data) => {
       localStorage.setItem('sessionId', data.session_id);
@@ -49,32 +49,32 @@ const ChatView: React.FC = () => {
       console.log(`Session restorée avec l'ID: ${data.session_id}`);
     });
 
-	socket.on('response_start', () => {
-		console.log('Début de la réponse');
-		setMessages((prevMessages) => [
-			...prevMessages,
-			{ type: MessageType.Bot, content: '', status: MessageStatus.Ongoing },
-		]);
-		setIsReciving(true);
-	});
+    socket.on('response_start', () => {
+      console.log('Début de la réponse');
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: MessageType.Bot, content: '', status: MessageStatus.Ongoing },
+      ]);
+      setIsReciving(true);
+    });
 
-	socket.on('response', (message) => {
-		setMessages((prevMessages) =>
-			prevMessages.map((msg) =>
-				msg.status === MessageStatus.Ongoing ? { ...msg, content: msg.content + message } : msg
-			)
-		);
-	});
+    socket.on('response', (message) => {
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg.status === MessageStatus.Ongoing ? { ...msg, content: msg.content + message } : msg
+        )
+      );
+    });
 
-	socket.on('response_end', () => {
-		console.log('Fin de la réponse');
-		setMessages((prevMessages) =>
-			prevMessages.map((msg) =>
-				msg.status === MessageStatus.Ongoing ? { ...msg, status: MessageStatus.Finished } : msg
-			)
-		);
-		setIsReciving(false);
-	});
+    socket.on('response_end', () => {
+      console.log('Fin de la réponse');
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg.status === MessageStatus.Ongoing ? { ...msg, status: MessageStatus.Finished } : msg
+        )
+      );
+      setIsReciving(false);
+    });
 
     socket.on('disconnect', () => {
       console.log('Déconnecté du serveur');
@@ -86,77 +86,79 @@ const ChatView: React.FC = () => {
   }, [sessionId]);
 
   const handleSendMessage = useCallback((e: React.FormEvent) => {
-	e.preventDefault();
-	if (!newMessage.trim()) return;
-	if (sessionId) {
-		socketRef.current?.emit('query', { question: newMessage, session_id: sessionId });
-		setMessages((prevMessages) => [
-			...prevMessages,
-			{ type: MessageType.User, content: newMessage },
-		]);
-		setNewMessage('');
-	} else {
-		console.error('Session ID is not set.');
-	}
+    e.preventDefault();
+    if (!newMessage.trim()) return;
+    if (sessionId) {
+      socketRef.current?.emit('query', { question: newMessage, session_id: sessionId });
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: MessageType.User, content: newMessage },
+      ]);
+      setNewMessage('');
+    } else {
+      console.error('Session ID is not set.');
+    }
   }, [sessionId, newMessage]);
 
   const scrollToLastMessage = useCallback(() => {
-	if (lastMessageRef.current) {
-	  lastMessageRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-	}
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
   }, []);
 
   useEffect(() => {
-	scrollToLastMessage();
+    scrollToLastMessage();
   }, [messages, scrollToLastMessage]);
-  
+
   useEffect(() => {
-	observerRef.current = new IntersectionObserver((entries) => {
-		const entry = entries[0];
-		setIsLastMessageInView(entry.isIntersecting);
-	});
+    observerRef.current = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setIsLastMessageInView(entry.isIntersecting);
+    });
 
-	const currentLastMessageRef = lastMessageRef.current;
+    const currentLastMessageRef = lastMessageRef.current;
 
-	if (currentLastMessageRef) {
-		observerRef.current.observe(currentLastMessageRef);
-	}
+    if (currentLastMessageRef) {
+      observerRef.current.observe(currentLastMessageRef);
+    }
 
-	return () => {
-		if (observerRef.current && currentLastMessageRef) {
-			observerRef.current.unobserve(currentLastMessageRef);
-		}
-	};
-	}, [messages]);
-  
+    return () => {
+      if (observerRef.current && currentLastMessageRef) {
+        observerRef.current.unobserve(currentLastMessageRef);
+      }
+    };
+  }, [messages]);
+
   return (
-    <div className="chat-container">
-		<div className="chat-message-list">
-		<ul className="messages-list">
-			{messages.map((message, index) => (
-			<Message
-				key={index}
-				type={message.type}
-				content={message.content}
-				ref={index === messages.length - 1 ? lastMessageRef : null}
-			/>
-			))}
-		</ul>
-	  {!isLastMessageInView && (
-			<div className="scroll-button-container">
+    <Box className="chat-container">
+      <Box className="chat-message-list">
+        <ul className="messages-list">
+          {messages.map((message, index) => (
+            <Message
+              key={index}
+              type={message.type}
+              content={message.content}
+              ref={index === messages.length - 1 ? lastMessageRef : null}
+            />
+          ))}
+        </ul>
+        {!isLastMessageInView && (
+			<Box className="scroll-button-container">
 				<IconButton onClick={scrollToLastMessage} className="scroll-button">
-					<ArrowDownwardIcon sx={{ color: 'white' }} />
+				<ArrowDownwardIcon sx={{ color: 'white' }} />
 				</IconButton>
-			</div>
-		)}
-		</div>
-      <form onSubmit={handleSendMessage} className="message-form">
-        <textarea
+			</Box>
+        )}
+      </Box>
+      <Box component="form" onSubmit={handleSendMessage} className="message-form">
+        <TextareaAutosize
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Type a message..."
+          placeholder="Message..."
           className="message-input"
-		  rows={1}
+		  minRows={1}
+		  maxRows={3}
+          style={{ flexGrow: 1, borderRadius: 20, padding: 10, border: '1px solid #ccc' }}
         />
         <IconButton
           type="submit"
@@ -167,15 +169,15 @@ const ChatView: React.FC = () => {
               backgroundColor: isReciving ? '#cccccc' : '#0056b3',
             },
             color: 'white',
-            width: '50px',
+			width: '50px',
             height: '50px',
-			cursor: isReciving ? 'not-allowed' : 'pointer',
+            cursor: isReciving ? 'not-allowed' : 'pointer',
           }}
         >
           <SendIcon style={{ color: 'white' }} />
         </IconButton>
-      </form>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
