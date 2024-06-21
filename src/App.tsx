3ac580180536a1f8ce78e8b4c3c763131f349f93
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-21 11:31:58 +0200

    update UI fully responsive with material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import { Container, Box, CssBaseline, AppBar, Toolbar, Typography, Grid } from '@mui/material';
-import ChatView from './components/chatview/chatview';
-import './App.css';
+import React from "react";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  Box,
+} from "@mui/material";
+import ChatView from "./components/chatview/chatview";
+import Sidebar from "./components/sidebar/sidebar";
+import "./App.css";
+import logo from "./assets/UNIL_logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
 
 function App() {
+	const [showNavMenu, setShowNavMenu] = React.useState(false);
+
+	const handleOpenCloseNavMenu = () => {
+		setShowNavMenu(!showNavMenu);
+	}
+	
   return (
-    <div className="App">
-      <AppBar position="static">
+    <Box className="App">
+      <AppBar position="sticky" className="AppBar">
         <Toolbar>
-          <Typography variant="h6">
-            UNIL Chatbot
-          </Typography>
+			<IconButton
+				size="large"
+				edge="start"
+				color="inherit"
+				aria-label="menu"
+				sx={{ mr: 2 }}
+				onClick={handleOpenCloseNavMenu}
+			>
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="UNIL Logo" className="app-logo" />
         </Toolbar>
       </AppBar>
-	  <Container className="Chat-container" maxWidth="xl">
-	  	<CssBaseline />
-	  	<ChatView />
-	  </Container>
-    </div>
+      <Grid container className="App-container">
+	  	<Grid
+			item
+			xs={12}
+			sm={showNavMenu ? 4 : 0}
+			md={showNavMenu ? 3 : 0}
+			lg={showNavMenu ? 3 : 0}
+			xl={showNavMenu ? 2 : 0}
+			className={`Sidebar ${showNavMenu ? 'show' : 'hide'}`}
+		>
+			<Sidebar />
+		</Grid>
+		<Grid item xs={12} sm={showNavMenu ? 8 : 12} md={showNavMenu ? 9 : 12} lg={showNavMenu ? 9 : 12} xl={showNavMenu ? 10 : 12} className="Chat-container">
+			<ChatView />
+		</Grid>
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,42 @@
-// src/components/sidebar/sidebar.tsx
-
-import React from 'react';
+import React from "react";
+import {
+  Box,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import HelpIcon from "@mui/icons-material/Help";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import "./sidebar.css";
 
 const Sidebar = () => {
-    return <div>Sidebar component</div>;
+  return (
+    <Box className="sidebar-container">
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <ChatIcon />
+          </ListItemIcon>
+          <ListItemText primary="Nouveau Chat" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <HelpIcon />
+          </ListItemIcon>
+          <ListItemText primary="Aide" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <ContactMailIcon />
+          </ListItemIcon>
+          <ListItemText primary="Nous contacter" />
+        </ListItem>
+      </List>
+    </Box>
+  );
 };
 
 export default Sidebar;
