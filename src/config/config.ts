f6d45689e018ef87b9984bcff5f49473d45f1365
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-07-22 20:48:24 +0200

    update config frontend, relative path

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,6 @@
 // src/conf/conf.ts
 
 class Config {
-  	// public static readonly BASE_URL = `http://${process.env.REACT_APP_BACKEND_HOST}`;
-	// public static readonly API_URL = `${Config.BASE_URL}/api/v1`;
-	// public static readonly SOCKET_URL = `${Config.BASE_URL}`;
-
-	//make url relative
 	public static readonly SOCKET_URL = '/';
 	public static readonly API_URL = '/api/v1';
 	public static readonly BASE_URL = '/';
