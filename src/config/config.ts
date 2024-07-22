a1ab6e5564fa485c6a06c036a63ad5a4810dbf39
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-07-22 11:31:11 +0200

    test config relative

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,14 @@
 // src/conf/conf.ts
 
 class Config {
-  public static readonly BASE_URL = `http://${process.env.REACT_APP_BACKEND_HOST}`;
-  public static readonly API_URL = `${Config.BASE_URL}/api/v1`;
-  public static readonly SOCKET_URL = Config.BASE_URL;
+  	// public static readonly BASE_URL = `http://${process.env.REACT_APP_BACKEND_HOST}`;
+	// public static readonly API_URL = `${Config.BASE_URL}/api/v1`;
+	// public static readonly SOCKET_URL = `${Config.BASE_URL}`;
+
+	//make url relative
+	public static readonly SOCKET_URL = '/';
+	public static readonly API_URL = '/api/v1';
+	public static readonly BASE_URL = '/';
 }
 
 export default Config;
